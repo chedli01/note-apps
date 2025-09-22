a579f4b7516a2bf3a479ece8bf2b5aaac3894914
chedli01 <chedli.masmoudi01@gmail.com>
2025-09-22 19:09:00 +0100

    first component integration

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,10 @@
+import { Button } from "../components/ui/button";
+
 export default function Home(){
     return(
         <div>
             <h1>this is home page</h1>
+            <Button>ClickMe</Button>
         </div>
     )
 }
