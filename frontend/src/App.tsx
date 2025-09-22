434adf812efbc4098aaf11437ad38b708eef3095
chedli01 <chedli.masmoudi01@gmail.com>
2025-09-22 18:59:22 +0100

    signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/home'
 import Notes from './pages/notes'
 import Login from './pages/login'
+import Signup from './pages/signup'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Route element={<Home/>} path='home' />
       <Route element={<Notes/>} path='notes' />
       <Route element={<Login/>} path='login'/>
+      <Route element={<Signup/>} path='signup'/>
 
     
     </Routes>
