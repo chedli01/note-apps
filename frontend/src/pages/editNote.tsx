d0641355ff04963e7743609fe93dba05d165dfc4
chedli01 <chedli.masmoudi01@gmail.com>
2025-09-22 22:35:48 +0100

    edit note

--- frontend/src/pages/editNote.tsx
+++ frontend/src/pages/editNote.tsx
@@ -1,7 +1,10 @@
+import { useParams } from "react-router";
 import EditNoteForm from "../components/ui/editNoteForm";
 
 export default function EditNote(){
+    const { id } = useParams<{ id: string }>();
+
     return(
-        <EditNoteForm/>
+        <EditNoteForm id={id}/>
     )
 }
