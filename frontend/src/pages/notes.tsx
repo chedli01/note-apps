4e15f67bbd9ab2ccf1ba9b046e55bec2e5bf271d
chedli01 <chedli.masmoudi01@gmail.com>
2025-09-22 20:47:54 +0100

    notes state

--- frontend/src/pages/notes.tsx
+++ frontend/src/pages/notes.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import Note from "../components/ui/note";
 
+type Note = {
+    id: string,
+    title : string,
+}
+
 export default function Notes() {
+    const [notes,setNotes] = useState<Array<Note>>([{id:"1",title:"note monday"},{id:"2",title:"note tuesday"}])
     return (
         <div className="w-screen h-screen  flex items-center justify-center">
             <div className="w-1/2 h-1/2 ">
-                <Note title="vv"/>
+            {notes.map((element)=>{return <Note key={element.id} title={element.title}/> })}
+                
             </div>
         </div>
     )
